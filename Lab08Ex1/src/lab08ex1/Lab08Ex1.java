c3f0fd7dcbe6850477e8c325bea2989c354a5280
Leonardo Souza de Castro <82420617+Leonardo-Souza-de-Castro@users.noreply.github.com>
2024-09-28 08:41:58 -0300

    Add files via upload

---
+++ Lab08Ex1/src/lab08ex1/Lab08Ex1.java
@@ -0,0 +1,15 @@
+package lab08ex1;
+
+import view.Janela;
+
+
+public class Lab08Ex1 {
+
+    
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        
+        j.setVisible(true);
+    }
+    
+}
