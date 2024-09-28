b138f1b29ebce8202c68a98785bac25665c46b47
Giovanni Antonio Moreira <giovannimoreira131@gmail.com>
2024-09-28 08:41:06 -0300

    Add files via upload

---
+++ Lab08Exe01/src/lab08exe01/Lab08Exe01.java
@@ -0,0 +1,11 @@
+package lab08exe01;
+import view.Janela;
+
+public class Lab08Exe01 {
+
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setVisible(true);
+    }
+    
+}
