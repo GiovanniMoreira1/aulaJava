7b3c6389b517fe8b86b07ff9556ad78d96857622
Giovanni Antonio Moreira <giovannimoreira131@gmail.com>
2024-10-03 22:16:28 -0300

    Add files via upload

---
+++ aula09Exe01/src/aula09exe01/Aula09Exe01.java
@@ -0,0 +1,17 @@
+package aula09exe01;
+
+/**
+ *
+ * @author unifgimoreira
+ */
+public class Aula09Exe01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setVisible(true);
+    }
+    
+}
